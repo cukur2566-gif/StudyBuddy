b6d60ede264015847714442a200ec1d92796b5b4
cukur2566-gif <cukur2566@gmail.com>
2026-02-05 12:28:22 +0200

    Remove BillingSystem class and notification method
    Removed the BillingSystem class and its notifyRegistration method.

--- BillingSystem.java
+++ BillingSystem.java
@@ -1,10 +1,1 @@
-package entity;
 
-import java.util.List;
-
-public class BillingSystem {
-    public static void notifyRegistration(String studentId, List<String> courses) {
-        System.out.println("[Billing System] Notification: Student " + studentId 
-            + " registered in: " + courses);
-    }
-}
